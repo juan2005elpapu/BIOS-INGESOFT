220c28cf15f6a7c08d2de6d4e007e15f1970a963
juan2005elpapu <judapelo2005@gmail.com>
2026-02-09 14:18:01 -0500

    Add Heroku release and scaling script
    Add a Procfile release command to run migrations on deploy (release: python manage.py migrate). Introduce scripts/heroku_scale.py which uses the Heroku Platform API to scale the web formation based on Bogota time (1 dyno between 08:00–19:59, 0 otherwise); it requires HEROKU_APP_NAME and HEROKU_API_KEY env vars and the requests library. Also apply a minor whitespace/newline cleanup in Proyecto/batches/models.py.

--- Proyecto/batches/models.py
+++ Proyecto/batches/models.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 from batches.storage import SupabaseStorage
+
 
 def get_batch_storage():
     """Retorna SupabaseStorage si está habilitado."""
